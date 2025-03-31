eb60636f6f6aa0c077914e6b8701bf0fb93b88f4
tora <kawarusosu@zoho.com>
2025-04-01 03:20:42 +0900

    update-prefix: deactivated registry reset

--- update-prefix.sh
+++ update-prefix.sh
@@ -6,9 +6,11 @@ SAMPLEAPPID=1720850
 cd ~/wineprefix
 
 
+# UNCOMMENT here to reset registry files (which may be necessary occasionary idk)
+# rm -rf "$1/pfx/"*.reg
+# cp -Lr "$HOME/.local/share/Steam/steamapps/compatdata/$SAMPLEAPPID/pfx/"*.reg "$1/pfx"
+
 # for safety, copy windows files rather than linking them
-rm -rf "$1/pfx/"*.reg
-cp -Lr "$HOME/.local/share/Steam/steamapps/compatdata/$SAMPLEAPPID/pfx/"*.reg "$1/pfx"
 rm -rf "$1/pfx/drive_c/windows"
 cp -Lr "$HOME/.local/share/Steam/steamapps/compatdata/$SAMPLEAPPID/pfx/drive_c/windows" "$1/pfx/drive_c/windows"
 
